5aac6f372066d88137bf56944620857a51a4bc7e
jcrisolini <jcrisolini@consensusgroup.net>
2017-09-13 23:04:31 -0300

    no repeticion, y mensaje sen repro

--- interfaze/player.js
+++ interfaze/player.js
@@ -17,28 +17,41 @@ $(document).ready(function () {
         }
         clip = clips[0];
         clips.shift();
+        var nextClip = clips[0]
 
         $('video')[0].src = "clips/" + clip._id + ".mp4"
-
+        var duration = 3000 //randomInt(10, 20) * 1000
         if (clip.action.match(/Insert|Pullout/))
             tRepro = setTimeout(() => { repro(clips) }, clip.duration * 2 * 1000)
         else
-            tRepro = setTimeout(() => { repro(clips) }, 3000)//randomInt(10, 20) * 1000)
+            tRepro = setTimeout(() => { repro(clips) }, duration)//randomInt(10, 20) * 1000)
+        if (clip.message)
+            showText(clip.message)
+
+        if (!nextClip)
+            return
+
+        var message = ''
+        if (nextClip.prepare)
+            message += nextClip.prepare
+        if (nextClip.position != clip.position)
+            message += positions[nextClip.position].prepare
+        if (nextClip.prostate && !clip.prostate)
+            message += "Prepare to hit your prostate"
+        if (nextClip.hard && !clip.hard)
+            message += (["let's ready for go crazy", "go hard soon"])[randomInt(0, 1)]
     }
+
     var lastClip = -1
     var stageClips = []
-    function fucking(stage) {
-
-        if (stage == 1 && !stageClips.length)
-            stageClips = clips.filter((c) => { return (!c.hard && !c.prostate && c.deep != "ReallyDeep" && !c.deep != "Deep") })
-        index = randomInt(0, stageClips.length - 1)
-        while (index == lastClip)
-            index = randomInt(0, stageClips.length)
-        lastClip = index
-        $('video')[0].src = "clips/" + stageClips[index]._id + ".mp4"
-
-
-        tRepro = setTimeout(function () { execute(stage) }, randomInt(10, 20) * 1000)
+    function showText(message, inTime) {
+        inTime = inTime || 0;
+        setTimeout(function () {
+            //show
+        }, inTime)
+        setTimeout(function () {
+            //hide
+        }, inTime + (8 * 1000))
     }
     function randomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
